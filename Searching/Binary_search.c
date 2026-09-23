9ecb10f811c0b2828b5d88aed5e244708b41a0bb
Vivanjain08 <vivan.jain.cse.2025@miet.ac.in>
2026-09-23 12:00:31 +0530

    Add files via upload

---
+++ Searching/Binary_search.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10], n, i, search;
+    int low, high, mid;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements in sorted order:\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Enter element to search: ");
+    scanf("%d", &search);
+
+    low = 0;
+    high = n - 1;
+
+    while(low <= high)
+    {
+        mid = (low + high) / 2;
+
+        if(a[mid] == search)
+        {
+            printf("Element found at position %d", mid + 1);
+            return 0;
+        }
+
+        if(search < a[mid])
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+
+    printf("Element not found");
+
+    return 0;
+}
